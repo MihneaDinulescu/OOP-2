6989a312dc01956207ed8cb7a0382c1e95fb173e
MihneaDinulescu <148634653+MihneaDinulescu@users.noreply.github.com>
2025-05-29 19:36:09 +0300

    Delete Proiect Vehicle Rental System directory

--- Proiect Vehicle Rental System/src/exceptions/ClientNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-
-public class ClientNotFoundException extends RuntimeException {
-    public ClientNotFoundException(long id) {
-        super("Client with ID " + id + " not found.");
-    }
-}
--- Proiect Vehicle Rental System/src/exceptions/LocatieNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-
-public class LocatieNotFoundException extends RuntimeException {
-    public LocatieNotFoundException(long id) {
-        super("Locatie with ID " + id + " not found.");
-    }
-}
